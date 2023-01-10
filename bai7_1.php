ce38a9e6c330d2ff3f9a1acee24913ae2e7fde53
DiemQuynh <haquynhla2002@gmail.com>
2023-01-10 12:16:33 +0700

    Upfile

---
+++ bai7_1.php
@@ -0,0 +1,6 @@
+<?php
+//get truyền dl từ người dùng lên server
+$user_name = isset($_GET["user_name"]) ? $_GET["user_name"]:"";
+echo $user_name;
+
+?>
---
+++ bai7_2.php
@@ -0,0 +1,7 @@
+<?php
+
+//post ẩn thông tin truyền lên
+$user_name = isset($_POST["user_name"]) ? $_POST["user_name"]:"";
+echo $user_name;
+
+?>
